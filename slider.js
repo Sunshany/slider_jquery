a446766c956bd5b9cb625c9640add1389fd2c5c1
Sarah Amairi <Sarah Amairi>
2017-01-15 23:05:04 +0100

    flèche précédent fait

--- slider.js
+++ slider.js
@@ -1,25 +1,36 @@
+const slidesCount = 5
 const slideShow = $('.gallery ul')
 const baseUrl = 'pictures/spiderman_'
 
-let i = 0
-let slidesContent = ''
-while (++i < 5) {
-  slidesContent += '<li class="each"><img src="'
-    + baseUrl + i +'.jpg" /></li>'
-}
-slideShow.append(slidesContent)
+slideShow.append(Array(slidesCount).fill().reduce(function (totalContent, _, i) {
+  return totalContent + '<li class="each"><img src="'
+    + baseUrl + (i + 1) +'.jpg" /></li>'
+}, ''))
 
-const firstSlide = slideShow.find('li.each').first()
+const allSlides = slideShow.find('li.each')
+const firstSlide = allSlides.first()
+const lastSlide = allSlides.last()
 
 firstSlide.addClass('active').show()
 
-$('.direction .next').click(function() {
+$('.direction-next').click(function() {
   const nextSlide = slideShow.find('li.active').next()
 
   slideShow.find('li.active').removeClass('active')
-  if (!nextSlide.length) {
-    firstSlide.addClass('active')
-  } else {
+  if (nextSlide && nextSlide.length) {
     nextSlide.addClass('active')
+  } else {
+    firstSlide.addClass('active')
+  }
+})
+
+$('.direction-prev').click(function() {
+  const prevSlide = slideShow.find('li.active').prev()
+
+  slideShow.find('li.active').removeClass('active')
+  if (prevSlide && prevSlide.length) {
+    prevSlide.addClass('active')
+  } else {
+    lastSlide.addClass('active')
   } 
 })
