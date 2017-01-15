ef7f7310c9a92d3cda070cff11c1fa9b84fdc335
Sarah Amairi <Sarah Amairi>
2017-01-15 22:43:39 +0100

    bouton suivant et animation faite

--- slider.js
+++ slider.js
@@ -1,7 +1,25 @@
-$(document).ready(function(){
-	$('.main_content').append('<img id="spiderman_1" src="pictures/spiderman_1.jpg" />')
-	$('.main_content').append('<img id="spiderman_1" src="pictures/spiderman_2.jpg" />')
-	$('.main_content').append('<img id="spiderman_1" src="pictures/spiderman_3.jpg" />')
-	$('.main_content').append('<img id="spiderman_1" src="pictures/spiderman_4.jpg" />')
-	$('.main_content').append('<img id="spiderman_1" src="pictures/spiderman_5.jpg" />')	
-});
+const slideShow = $('.gallery ul')
+const baseUrl = 'pictures/spiderman_'
+
+let i = 0
+let slidesContent = ''
+while (++i < 5) {
+  slidesContent += '<li class="each"><img src="'
+    + baseUrl + i +'.jpg" /></li>'
+}
+slideShow.append(slidesContent)
+
+const firstSlide = slideShow.find('li.each').first()
+
+firstSlide.addClass('active').show()
+
+$('.direction .next').click(function() {
+  const nextSlide = slideShow.find('li.active').next()
+
+  slideShow.find('li.active').removeClass('active')
+  if (!nextSlide.length) {
+    firstSlide.addClass('active')
+  } else {
+    nextSlide.addClass('active')
+  } 
+})
