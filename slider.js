c044f6177d715e4b256146491a1b95a7fbad6a16
Sarah Amairi <Sarah Amairi>
2017-01-15 20:42:40 +0100

    ajout des images via jQuery

--- slider.js
+++ slider.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
-	$('.your-class').slick({
-		setting-name: setting-value
-	});
+	$('.main_content').append('<img id="spiderman_1" src="pictures/spiderman_1.jpg" />')
+	$('.main_content').append('<img id="spiderman_1" src="pictures/spiderman_2.jpg" />')
+	$('.main_content').append('<img id="spiderman_1" src="pictures/spiderman_3.jpg" />')
+	$('.main_content').append('<img id="spiderman_1" src="pictures/spiderman_4.jpg" />')
+	$('.main_content').append('<img id="spiderman_1" src="pictures/spiderman_5.jpg" />')	
 });
